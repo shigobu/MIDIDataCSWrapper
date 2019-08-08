b7451f1b3ebeec27a7403ea5297d20891c123ea8
shigobu <shigobu@shigobu.com>
2019-08-08 14:24:45 +0900

    midiデータクラスを変更

---
+++ MIDIDataCSWrapper/MIDIData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDIDataCSWrapper
+{
+    /// <summary>
+    /// MIDIデータは、フォーマット、トラック数、タイムベース(タイムモード及び分解能)の情報を持ち、子に単数又は複数のトラックを持つことができる。
+    /// </summary>
+    public class MIDIData
+    {
+        /// <summary>
+        /// MIDIデータのフォーマット
+        /// </summary>
+        public enum Format
+        {
+            Format0 = 0,
+            Format1 = 1,
+            Format2 = 2
+        }
+
+        /// <summary>
+        /// MIDIデータのタイムモード
+        /// </summary>
+        public enum TimeMode
+        {
+            TPQN = 0,
+            SMPTE24 = 24,
+            SMPTE25 = 25,
+            SMPTE29 = 29,
+            SMPTE30 = 30
+        }
+
+        public MIDIData(Format format, int numTrack, TimeMode timeMode, int timeResolution)
+        {
+
+        }
+    }
+}
