475da3e036a8ebd8411aadc4e781f39b15c42d14
shigobu <shigobu@shigobu.com>
2019-08-16 10:27:57 +0900

    IDisposableの実装を削除 ファイナライザにメモリ解放処理を作成

--- MIDIDataCSWrapper/MIDIData.cs
+++ MIDIDataCSWrapper/MIDIData.cs
@@ -11,7 +11,7 @@ namespace MIDIDataCSWrapper
     /// <summary>
     /// MIDIデータは、フォーマット、トラック数、タイムベース(タイムモード及び分解能)の情報を持ち、子に単数又は複数のトラックを持つことができる。
     /// </summary>
-    public class MIDIData : IDisposable
+    public class MIDIData
     {
         #region DLLImport
 
@@ -1095,43 +1095,12 @@ namespace MIDIDataCSWrapper
 		}
 		#endregion
 
-		#region IDisposable Support
-		private bool disposedValue = false; // 重複する呼び出しを検出するには
-
-        protected virtual void Dispose(bool disposing)
+		#region ファイナライザー
+		~MIDIData()
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)。
-                }
-
-                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
-                // TODO: 大きなフィールドを null に設定します。
-
-                MIDIData_Delete(UnManagedObjectPointer);
-                UnManagedObjectPointer = IntPtr.Zero;
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: 上の Dispose(bool disposing) にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
-        ~MIDIData()
-        {
-            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
-            Dispose(false);
-        }
-
-        // このコードは、破棄可能なパターンを正しく実装できるように追加されました。
-        void IDisposable.Dispose()
-        {
-            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
-            Dispose(true);
-            // TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
-            GC.SuppressFinalize(this);
-        }
+			MIDIData_Delete(UnManagedObjectPointer);
+			UnManagedObjectPointer = IntPtr.Zero;
+		}
         #endregion
     }
 }
