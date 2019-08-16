89723d29bdd019215184d46dfc2579c0d353cfe5
shigobu <shigobu@shigobu.com>
2019-08-16 16:22:14 +0900

    構造体をまとめた、ファイルを追加

---
+++ MIDIDataCSWrapper/structs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDIDataCSWrapper
+{
+	/// <summary>
+	/// 小節：拍：ティックを表します
+	/// </summary>
+	public struct MeasureBeatTick
+	{
+		/// <summary>
+		/// 小節：拍：ティックを指定して、オブジェクトの初期化をします。
+		/// </summary>
+		/// <param name="measure">小節</param>
+		/// <param name="beat">拍</param>
+		/// <param name="tick">ティック</param>
+		public MeasureBeatTick(int measure, int beat, int tick) : this()
+		{
+			Measure = measure;
+			Beat = beat;
+			Tick = tick;
+		}
+		/// <summary>
+		/// 小節
+		/// </summary>
+		public int Measure { get; set; }
+		/// <summary>
+		/// 拍
+		/// </summary>
+		public int Beat { get; set; }
+		/// <summary>
+		/// ティック
+		/// </summary>
+		public int Tick { get; set; }
+	}
+
+
+}
