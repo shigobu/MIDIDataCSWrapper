b867c5ee216aa23ff50be022214c762a0b70a477
shigenobu <shigenobu314@yahoo.co.jp>
2019-09-03 22:07:54 +0900

    テストアプリを追加

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIDIDataCSWrapper;
+
+namespace TestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MIDIData mIDIData = new MIDIData(@"C:\Users\笠松茂信\Desktop\無題1.skj");
+			foreach (MIDITrack item in mIDIData)
+			{
+				Console.WriteLine(item.ForeColor.ToString());
+			}
+
+			Console.Read();
+		}
+	}
+}
