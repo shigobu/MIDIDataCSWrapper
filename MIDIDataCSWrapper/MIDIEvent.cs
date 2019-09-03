8a924a0704aa370b97fe4daddd2a585290d1fb18
shigenobu <shigenobu314@yahoo.co.jp>
2019-09-03 23:05:08 +0900

    Kinds列挙体作成

--- MIDIDataCSWrapper/MIDIEvent.cs
+++ MIDIDataCSWrapper/MIDIEvent.cs
@@ -64,6 +64,40 @@ namespace MIDIDataCSWrapper
 			SMPTE30N = 0x03 
 		}
 
+		/// <summary>
+		/// MIDIイベントの種類
+		/// </summary>
+		public enum Kinds
+		{
+			SequenceNumber    = 0x00 ,
+			TextEvent         = 0x01 ,
+			CopyrightNotice   = 0x02 ,
+			TrackName         = 0x03 ,
+			InstrumentName    = 0x04 ,
+			Lyric             = 0x05 ,
+			Marker            = 0x06 ,
+			CuePoint          = 0x07 ,
+			ProgramName       = 0x08 ,
+			DeviceName        = 0x09 ,
+			ChannelPrefix     = 0x20 ,
+			PortPrefix        = 0x21 ,
+			EndOfTrack        = 0x2F ,
+			Tempo             = 0x51 ,
+			SmpteOffSet       = 0x54 ,
+			TimeSignature     = 0x58 ,
+			KeySignature      = 0x59 ,
+			SequencerSpecific = 0x7F ,
+			NoteOff           = 0x80 ,
+			NoteOn            = 0x90 ,
+			KeyAfterTouch     = 0xA0 ,
+			ControlChange     = 0xB0 ,
+			ProgramChange     = 0xC0 ,
+			ChannelAfterTouch = 0xD0 ,
+			PitchBend         = 0xE0 ,
+			SysExStart        = 0xF0 ,
+			SysExContinue     = 0xF7 
+		}
+
 		#endregion
 
 		#region プロパティ
