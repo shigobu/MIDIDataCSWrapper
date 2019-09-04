1e0253c87f0f08984924d403964ce0fd2aff67ed
shigobu <shigobu@shigobu.com>
2019-09-04 13:50:59 +0900

    デスクトップのパス取得メソッドを使用知るように変更

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using MIDIDataCSWrapper;
 
 namespace TestApp
@@ -11,7 +12,7 @@ namespace TestApp
 	{
 		static void Main(string[] args)
 		{
-			MIDIData mIDIData = new MIDIData(@"C:\Users\笠松茂信\Desktop\無題1.skj");
+			MIDIData mIDIData = new MIDIData(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "無題1.skj"));
 			foreach (MIDITrack item in mIDIData)
 			{
 				Console.WriteLine(item.ForeColor.ToString());
